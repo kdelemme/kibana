d847ab0a96e8c167b15233b89903f20994089cc2
Kevin Delemme <kdelemme@gmail.com>
2026-04-24 09:49:12 -0400

    Fix workflow badge

--- x-pack/platform/plugins/shared/alerting_v2/public/components/action_policy/details_flyout/destination_row.tsx
+++ x-pack/platform/plugins/shared/alerting_v2/public/components/action_policy/details_flyout/destination_row.tsx
@@ -7,7 +7,6 @@
 
 import { EuiBadge, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import type { ActionPolicyDestination } from '@kbn/alerting-v2-schemas';
-import { FormattedMessage } from '@kbn/i18n-react';
 import React from 'react';
 import { WorkflowDestinationLink } from '../workflow_destination_link';
 
@@ -21,14 +20,8 @@ export const DestinationRow = ({ destination }: Props) => {
       <EuiFlexGroup gutterSize="s" alignItems="center" responsive={false}>
         <EuiFlexItem grow={false}>
           <EuiBadge color="hollow" iconType="workflow">
-            <FormattedMessage
-              id="xpack.alertingV2.actionPolicy.detailsFlyout.destination.workflow"
-              defaultMessage="Workflow"
-            />
+            <WorkflowDestinationLink id={destination.id} />
           </EuiBadge>
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <WorkflowDestinationLink id={destination.id} />
         </EuiFlexItem>
       </EuiFlexGroup>
     );
